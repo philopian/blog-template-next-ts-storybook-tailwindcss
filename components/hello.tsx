ac8df323e1a6d3048ce2f335cc63669ce5dcfb1b
philopian <pdotwdot@gmail.com>
2022-01-11 20:21:55 -0800

    Added tailwindcss

--- components/hello.tsx
+++ components/hello.tsx
@@ -2,6 +2,10 @@ type Props = {
   message: string
 }
 
+const styles = {
+  wrapper:
+    'bg-gray-100 border border-gray-200 dark:border-gray-700 rounded-md dark:bg-gray-900 text-sm text-red-600 p-4',
+}
 export default function Hello({ message }: Props) {
-  return <div>{message}</div>
+  return <div className={styles.wrapper}>{message}</div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Hello from '@/components/hello'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -19,6 +20,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Hello message="hello there!!" />
 
         <p className={styles.description}>
           Get started by editing{' '}
