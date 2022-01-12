7b7a9e0164f3d24c9ccdb374640dc13a461c5ed4
philopian <pdotwdot@gmail.com>
2022-01-11 20:22:51 -0800

    css modules

--- components/hello.tsx
+++ components/hello.tsx
@@ -1,11 +1,8 @@
+import styles from './hello.module.css'
 type Props = {
   message: string
 }
 
-const styles = {
-  wrapper:
-    'bg-gray-100 border border-gray-200 dark:border-gray-700 rounded-md dark:bg-gray-900 text-sm text-red-600 p-4',
-}
 export default function Hello({ message }: Props) {
   return <div className={styles.wrapper}>{message}</div>
 }
