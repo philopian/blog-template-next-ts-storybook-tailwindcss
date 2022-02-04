ace07f41246624f06142e57276f1a1e06cedeac6
philopian <pdotwdot@gmail.com>
2022-02-03 16:21:33 -0800

    cleanup the home page

--- components/Layout/Layout.test.tsx
+++ components/Layout/Layout.test.tsx
@@ -5,12 +5,13 @@ import Layout from './Layout'
 
 describe('Layout', () => {
   test('should render children data', () => {
+    const title = 'hello there'
     render(
-      <Layout>
+      <Layout title={title}>
         <p>...</p>
       </Layout>,
     )
-    const welcomeElement = screen.getByText(/Welcome to my blog/)
+    const welcomeElement = screen.getByText(/hello there/)
     expect(welcomeElement).toBeInTheDocument()
   })
 })
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,11 +2,16 @@ import Link from 'next/link'
 import { getCurrentYear } from '@/utils/dates'
 
 type Props = {
+  title: string
   children: JSX.Element[] | JSX.Element
 }
 
 type HomeButtonProp = {
   path?: string
+}
+
+type HeaderProps = {
+  title: string
 }
 
 function HomeButton({ path = '/' }: HomeButtonProp) {
@@ -17,19 +22,31 @@ function HomeButton({ path = '/' }: HomeButtonProp) {
   )
 }
 
-export default function Layout({ children }: Props) {
+function Header({ title }: HeaderProps) {
+  return (
+    <header className="bg-pink-600 text-slate-50 mb-8 py-4">
+      <div className="container mx-auto flex justify-center">
+        <HomeButton />
+        <span className="mx-auto">{title}</span>{' '}
+      </div>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="bg-pink-600 text-slate-50 mt-8 py-4">
+      <div className="container mx-auto flex justify-center">&copy; {getCurrentYear()} </div>
+    </footer>
+  )
+}
+
+export default function Layout({ title, children }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-pink-600 text-slate-50 mb-8 py-4">
-        <div className="container mx-auto flex justify-center">
-          <HomeButton />
-          <span className="mx-auto">Welcome to my blog</span>{' '}
-        </div>
-      </header>
+      <Header title={title} />
       <main className="container mx-auto flex-1">{children}</main>
-      <footer className="bg-pink-600 text-slate-50 mt-8 py-4">
-        <div className="container mx-auto flex justify-center">&copy; {getCurrentYear()} </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
